22884105a07121867a30ad77d714cd2aaf42c128
Nick Lindau <nicholaithegreat@gmail.com>
2021-08-01 13:18:10 -0700

    add menu fade animation

--- script.js
+++ script.js
@@ -8,7 +8,12 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
+const nav = document.querySelector('.nav');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
 const section1 = document.querySelector('#section--1');
+
 
 const openModal = function (e) {
   e.preventDefault();
@@ -34,7 +39,6 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-
 btnScrollTo.addEventListener('click', (e) => {
   e.preventDefault();
   section1.scrollIntoView({behavior: "smooth"}) //only works in modern browsers
@@ -52,9 +56,7 @@ document.querySelector('.nav__links').addEventListener('click', (e) => {
 });
 
 // Tabbed Component
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
+
 
 tabsContainer.addEventListener('click', (e) => {
   const clicked = e.target.closest('.operations__tab');
@@ -65,8 +67,26 @@ tabsContainer.addEventListener('click', (e) => {
   // Active Tab
   tabs.forEach(tab => tab.classList.remove('operations__tab--active'));
   clicked.classList.add('operations__tab--active'); 
-
+  
   // Activate Content Area
   console.log(clicked.dataset.tab);
   document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
 });
+
+// Menu Fade Animation
+
+const handleHover = function(e, opacity) {
+  if(e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img') // searching for any img element in the nav element
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = opacity;
+      });
+      logo.style.opacity = opacity;
+    }};
+
+nav.addEventListener('mouseover', e => handleHover(e, 0.5));
+
+nav.addEventListener('mouseout', e => handleHover(e, 1));
