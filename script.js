2db7bd30667740e2b6989d73742516826aff5720
Nick Lindau <nicholaithegreat@gmail.com>
2021-07-31 21:11:47 -0700

    add page navigation for nav bar links

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -32,10 +34,18 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
 
 btnScrollTo.addEventListener('click', (e) => {
   e.preventDefault();
   section1.scrollIntoView({behavior: "smooth"}) //only works in modern browsers
+});
+
+// Page Navigation
+
+document.querySelector('.nav__links').addEventListener('click', (e) => {
+  if (e.target.classList.contains('nav__link')) {
+    e.preventDefault();
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({behavior: 'smooth'})
+  }
 });
