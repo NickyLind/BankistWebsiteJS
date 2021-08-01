ae0a37b7fb1d8a16e739957a722455ece1428f0e
Nick Lindau <nicholaithegreat@gmail.com>
2021-08-01 12:48:28 -0700

    create functionality for tabbed component in 'operations'

--- script.js
+++ script.js
@@ -43,9 +43,30 @@ btnScrollTo.addEventListener('click', (e) => {
 // Page Navigation
 
 document.querySelector('.nav__links').addEventListener('click', (e) => {
+  e.preventDefault();
   if (e.target.classList.contains('nav__link')) {
-    e.preventDefault();
     const id = e.target.getAttribute('href');
+    console.log(id);
     document.querySelector(id).scrollIntoView({behavior: 'smooth'})
   }
+});
+
+// Tabbed Component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', (e) => {
+  const clicked = e.target.closest('.operations__tab');
+  console.log(clicked);
+  if(!clicked) return;
+  tabsContent.forEach(content => content.classList.remove('operations__content--active')); 
+
+  // Active Tab
+  tabs.forEach(tab => tab.classList.remove('operations__tab--active'));
+  clicked.classList.add('operations__tab--active'); 
+
+  // Activate Content Area
+  console.log(clicked.dataset.tab);
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
 });
