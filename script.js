512c1e0d58dd9acf7532aba829c889e6d3da6ea2
Nick Lindau <nicholaithegreat@gmail.com>
2021-08-02 12:34:17 -0700

    add functionality for lazy loading images

--- script.js
+++ script.js
@@ -134,3 +134,23 @@ allSections.forEach(function(section) {
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
 });
+
+// Lazy Loading Images
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = entries => {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return
+  entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener('load', () => {
+    entry.target.classList.remove('lazy-img');
+  });
+  observer.unobserve(entry.target);
+};
+
+imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
